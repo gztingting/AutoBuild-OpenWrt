40e3d2f539fad8274961f1dafe7abe159fd9a43f
Dingcon <gztingting@gmail.com>
2021-06-16 21:49:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,7 @@
 
 pushd package/lean
 git clone https://github.com/gztingting/luci-theme-argon-dark-mod
+git clone https://github.com/kenzok8/luci-theme-ifit
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-fileassistant
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser
 git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
