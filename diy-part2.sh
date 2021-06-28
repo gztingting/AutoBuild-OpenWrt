06fdb0b3feb54767b8b4b097f017cb33bc058c75
Dingcon <gztingting@gmail.com>
2021-06-28 15:13:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,6 +28,6 @@ sed -i 's/京东签到服务/JD-DailyBonus/g' package/lean/luci-app-jd-dailybonu
 #sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0/$1$PhflQnJ1$yamWfH5Mphs4hXV7UXWQ21:18725/g' $ZZZ  # 替换密码（要替换密码就不能设置密码为空）
 
 #sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                            # 设置密码为空
-sed -i '#V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0#UHJIZWtG$UqLZWUUXnhU16w.g5U/Ep1:18462#d' $ZZZ
+sed -i 's#V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0#UHJIZWtG$UqLZWUUXnhU16w.g5U/Ep1:18462#g' $ZZZ
 
 #sed -i 's/PATCHVER:=5.4/PATCHVER:=4.19/g' target/linux/x86/Makefile                              # 修改内核版本为4.19
