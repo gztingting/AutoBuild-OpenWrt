aa1f9db7da4c51aded2cd4921a265c066a68596b
Dingcon <gztingting@gmail.com>
2021-06-30 20:17:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-#
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
+## This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
+## https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
@@ -19,7 +16,7 @@ pushd package/lean
 git clone https://github.com/gztingting/luci-theme-argon-dark-mod
 git clone https://github.com/kenzok8/luci-theme-ifit
 #git clone https://github.com/gztingting/luci-app-fileassistant
-#svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-fileassistant
+svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-fileassistant
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser
 git clone https://github.com/sirpdboy/luci-app-autotimeset
 git clone https://github.com/fw876/helloworld
