d1b657bbf1c136e603784f32d394a22c24f5abad
Dingcon <gztingting@gmail.com>
2021-07-01 17:01:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,10 +27,5 @@ sed -i "s/OpenWrt /FlyStation $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ   
 
 sed -i "/uci commit system/i\uci set system.@system[0].hostname='FlyStation'" $ZZZ            # 修改主机名称为FlyStation
 sed -i 's/京东签到服务/JD-DailyBonus/g' package/lean/luci-app-jd-dailybonus/luasrc/controller/jd-dailybonus.lua #修改中文为英文
-#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0/$1$PhflQnJ1$yamWfH5Mphs4hXV7UXWQ21:18725/g' $ZZZ  # 替换密码（要替换密码就不能设置密码为空）
 
-#sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                            # 设置密码为空
-sed -i "s#root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::#root:$1$E.WtCyzS$ytUZFpkmn0ly4MeHkj02C/:18807:0:99999:7:::#g" $ZZZ
-
-#sed -i 's/PATCHVER:=5.4/PATCHVER:=4.19/g' target/linux/x86/Makefile                              # 修改内核版本为4.19
 chmod 755 package/lean/luci-app-autotimeset/root/etc/init.d/autotimeset
