138ec7ab2171425aa4deaf7ff5985ca3affa50eb
Dingcon <gztingting@gmail.com>
2023-01-10 22:12:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,7 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-rm -R ./package/feeds/luci/luci-app-aliyundrive-webdav
+rm -R ./feeds/luci/applications/luci-app-aliyundrive-webdav
 
 pushd package/lean
 git clone https://github.com/gztingting/luci-theme-argon-dark-mod
