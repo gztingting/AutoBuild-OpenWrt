dc45e0c068af5112ea8e8f653f5c0580ef125727
Dingcon <gztingting@gmail.com>
2023-01-16 09:02:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,7 +25,7 @@ svn co https://github.com/281677160/openwrt-package/trunk/cdnspeedtest
 svn co https://github.com/281677160/openwrt-package/trunk/luci-app-cloudflarespeedtest
 #svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser
 #git clone https://github.com/sirpdboy/luci-app-autotimeset
-git clone https://github.com/fw876/helloworld
+#git clone https://github.com/fw876/helloworld
 #svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman
 #git clone https://github.com/awesome-openwrt/luci-app-syncthing
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus
