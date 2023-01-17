bc4e5b1a56e6a0e1b5e1a99f137e454d0dd1a346
Dingcon <gztingting@gmail.com>
2023-01-17 11:29:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 
 #rm -R ./feeds/luci/applications/luci-app-aliyundrive-webdav
 
-pushd package/lean
+pushd feeds/luci/applications
 git clone https://github.com/gztingting/luci-theme-argon-dark-mod
 #git clone https://github.com/kenzok8/luci-theme-ifit
 git clone https://github.com/DevOpenWRT-Router/luci-app-rebootschedule
@@ -25,7 +25,7 @@ svn co https://github.com/281677160/openwrt-package/trunk/cdnspeedtest
 svn co https://github.com/281677160/openwrt-package/trunk/luci-app-cloudflarespeedtest
 #svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser
 #git clone https://github.com/sirpdboy/luci-app-autotimeset
-#git clone https://github.com/fw876/helloworld
+git clone https://github.com/fw876/helloworld
 #svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman
 #git clone https://github.com/awesome-openwrt/luci-app-syncthing
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus
